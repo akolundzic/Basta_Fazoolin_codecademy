c98138d0460412feb12179c15fa32fd64a19ea92
chinch42 <vanessakolund@gmail.com>
2022-12-14 23:10:07 +0100

    update read.me file and main.py

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Basta_Fazoolin_codecademy
+# Basta Fazoolin'Codecademy
+This is an implementation of an exersice from the codecadeym Python3 course. This is my own suggestions might deviate from the steps suggested in this exersice. 
+## Description
+You’ve started a position as the lead programmer for the family-style Italian restaurant Basta Fazoolin’ with My Heart. The restaurant has been doing fantastically and seen a lot of growth lately. You’ve been hired to keep things organized.
+### suggested steps
+1. At Basta Fazoolin’ with my Heart our motto is simple: when you’re here with family, that’s great! We have four different menus: brunch, early-bird, dinner, and kids. Create a Menu class .
+2. Give Menu a constructor with the five parameters self, name, items, start_time, and end_time.
+3. Let’s create our first menu: brunch. Brunch is served from 11am to 4pm. 
+4. Let’s create our second menu item early_bird. Early-bird Dinners are served from 3pm to 6pm. 
+5. Let’s create our third menu, dinner. Dinner is served from 5pm to 11pm. 
+6. And let’s create our last menu, kids. The kids menu is available from 11am until 9pm. The following items are available on the kids menu.
+7. Give our Menu class a string representation method that will tell you the name of the menu. Also, indicate in this representation when the menu is available.
+8. Try out our string representation. If you call print(brunch) it should print out something like the following:
+```
+  brunch menu available from 11am to 4pm
+```
+9. Give Menu a method .calculate_bill() that has two parameters: self, and purchased_items, a list of the names of purchased items. Have calculate_bill return the total price of a purchase consisting of all the items in purchased_items.
+10. Test out Menu.calculate_bill(). We have a breakfast order for one order of pancakes, one order of home fries, and one coffee. Pass that into brunch.calculate_bill() and print out the price.
+11. What about an early-bird purchase? Our last guests ordered the salumeria plate and the vegan mushroom ravioli. Calculate the bill with .calculate_bill().
+12. Basta Fazoolin’ with my Heart has seen tremendous success with the family market, which is fantastic because when you’re at Basta Fazoolin’ with my Heart with family, that’s great! We’ve decided to create more than one restaurant to offer our fantastic menus, services, and ambience around the country. First, let’s create a Franchise class.
+13. Give the Franchise class a constructor. Take in an address, and assign it to self.address. Also take in a list of menus and assign it to self.menus.
+14. Let’s create our first two franchises! Our flagship store is located at "1232 West End Road" and our new installment is located at "12 East Mulberry Street". Pass in all four menus along with these addresses to define flagship_store and new_installment.
+15. Give our Franchises a string representation so that we’ll be able to tell them apart. If we print out a Franchise it should tell us the address of the restaurant.
+16. Let’s tell our customers what they can order! Give Franchise an .available_menus() method that takes in a time parameter and returns a list of the Menu objects that are available at that time.
+17. Let’s test out our .available_menus() method! Call it with 12 noon as an argument and print out the results.
+18. Let’s do another test! See what is printed if we call .available_menus() with 5pm as an argument and print out the results.
+19. Since we’ve been so successful building out a branded chain of restaurants, we’ve decided to diversify. We’re going to create a restaurant that sells arepas! First let’s define a Business class.
+20. Give Business a constructor. A Business needs a name and a list of franchises.
+21. Let’s create our first Business. The name is "Basta Fazoolin' with my Heart" and the two franchises are flagship_store and new_installment.
+22. Before we create our new business, we’ll need a Franchise and before our Franchise we’ll need a menu. The items for our Take a’ Arepa available from 10am until 8pm.
+23. Next let’s create our first Take a’ Arepa franchise! Our new restaurant is located at "189 Fitzgerald Avenue". Save the Franchise object to a variable called arepas_place.
+24. Now let’s make our new Business! The business is called "Take a' Arepa"!
+25. Congrats! You created a system of classes that help structure your code and perform all business requirements you need. Whenever we need a new feature we’ll have the well-organized code required to make developing and shipping it easy.
+
+
+
---
+++ main.py
@@ -0,0 +1,137 @@
+import sys
+# ---- Menu class declaration ------
+class Menu:
+# 1 add motto to class
+  motto = "At Basta Fazoolin’ with my Heart our motto is simple: when you’re here with family, that’s great! We have four different menus: brunch, early-bird, dinner, and kids."
+
+# 3 create first menu
+  brunch ={ 'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+}
+
+# 4 early bird menu
+  early_bird = {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}
+# 5 dinner menu
+  dinner = {'crostini with eggplant caponata': 13.00, 'caesar salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
+}
+# 6 kids menu
+  kids = {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+} 
+# 22 create arepa mennu
+  arepa = {'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50}
+# when are the menus avaialble
+  time_available ={'brunch':[11,16],'early_bird':[15,18],'dinner':[17,23], 'kids':[11,21],'arepa':[10,20]}
+
+# 2 create constructor
+  def __init__(self,name,start_time,end_time):
+    self.name = name
+    self.start_time = start_time
+    self.end_time = end_time
+    print(repr(self))
+# 8 --------------------
+  def __repr__(self):
+    info_string = "This is not availalbe, exit programm "
+    if(hasattr(self,self.name)):
+        start,start_time=self.convert_time_ampm(self.start_time)
+        end,end_time = self.convert_time_ampm(self.end_time)
+        info_string = f"{str.title(self.name)} menu is available from {start_time}{start} to {end_time}{end}"  
+    else: 
+      print(info_string)
+      sys.exit('')
+    return info_string
+
+# time string in am,pm
+  def convert_time_ampm(self,timein):
+    if timein <= 12: return('am',timein)
+    else: return('pm',timein-12)
+
+# 9 calculate bill
+  def calculate_bill(self,purchased_items):
+    sum = 0
+    obj = getattr(self,self.name)
+    for dish in purchased_items:
+        sum += obj.get(dish,0)
+    return round(sum,1)
+    
+  # get menu list
+  def get_menulist(self):
+    dic_menu = {'early_bird':self.early_bird,'dinner':self.dinner,'kids':self.kids,'brunch':self.brunch}
+    return dic_menu
+
+# ---- Franchise class declaration ------
+# 12 create class franchise
+class Franchise:
+# 13 constructor
+  def __init__(self,address,Menu):
+    self.address = address
+# 14 declare menu object, access its methods
+    self.menu = Menu
+    print(repr(self))
+
+# 15 show repr string
+  def __repr__(self):
+    return f"Restaurant's address is : {self.address}"
+
+  # 18 get available menus for time_in
+  def available_menus(self,time_in):
+    availalbe_menus =[]
+    for menu in self.menu.time_available:
+        [begin,end] = self.menu.time_available[menu]
+        if time_in in range(begin,end+1):
+          availalbe_menus.append(menu)
+    return availalbe_menus
+
+# print available menu list
+  def print_menu(self,time_in):
+    string_out = self.available_menus(time_in)
+    postfix,time = self.menu.convert_time_ampm(time_in)
+    if string_out:
+      return f"Available menus at {time}{postfix}: {','.join(string_out)}"
+    else: 
+      return "No menu is at this time available"
+
+# --- Business Class ---
+# 19,20,21
+class Business:
+  def __init__(self,name,franchises):
+    self.name = name
+    self.franchises = franchises
+    print(repr(self))
+  
+  def __repr__(self):
+     string_out ='franchises'
+     if type(list()) != type(self.franchises):
+       print('Pass the right list argument')
+       sys.exit('')
+     length = len(self.franchises)
+     if length == 1 : string_out='franchise'
+     return f"{self.name} has {length} {string_out}"
+
+# 10 test for brunch
+brunch_menu = Menu('brunch',11,15)
+brunch_items = ['pancakes', 'home','coffee']
+print(brunch_menu.calculate_bill(brunch_items))
+
+# 11 test for early bird
+early_bird = Menu('early_bird',15,23)
+early_bird_items = ['salumeria plate','mushroom ravioli (vegan)']
+print(early_bird.calculate_bill(early_bird_items))
+
+# 14 create two flagships, get all menus
+flagship_store = Franchise("1232 West End Road",early_bird)
+
+# 17 test menu at 12am
+print(flagship_store.print_menu(12))
+
+# 18 test menu at 17 o'clock, 5pm
+print(flagship_store.print_menu(17))
+print("\n")
+new_installment = Franchise("12 East Mulberry",brunch_menu)
+
+# 23 create arepa franchise
+arepa_place = Franchise("189 Fitzgerald Avenue",brunch_menu)
+franchises = [flagship_store,new_installment]
+name= "Basta Fazoolin\' with my Heart"
+business = Business(name,franchises)
+arepa_business = Business("Take a\' Arepa\'",[arepa_place])
+
